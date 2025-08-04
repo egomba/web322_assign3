ee85daf57976092d7e48b707bec4fc66b721e67d
egomba <egomba@myseneca.ca>
2025-08-04 17:48:02 -0400

    s

--- api/library.js
+++ api/library.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
-const sesh = require('express-session');
+const session = require('express-session');
 const rando = require('randomstring');
 const serverless = require('serverless-http');
 const { MongoClient, ObjectId } = require('mongodb');
@@ -11,14 +11,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const isVercel = !!process.env.VERCEL;
 
-const { router: borrowReturnRouter, setup: borrowReturnSetup } = require('../routes/borrowReturnRouter');
-
 // MongoDB
 const mongoUrl = 'mongodb+srv://egomba:Gomba123@egomba.ut79j.mongodb.net/?retryWrites=true&w=majority&appName=egomba';
 const dbName = 'Web322';
+
+let cachedClient = null;
+let cachedDb = null;
 async function connectToDatabase() {
+  if (cachedDb) return cachedDb;
   const client = await MongoClient.connect(mongoUrl);
-  return client.db(dbName);
+  cachedClient = client;
+  cachedDb = client.db(dbName);
+  return cachedDb;
+}
+
+// Load users
+let users = {};
+try {
+  users = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'user.json'), 'utf8'));
+} catch (err) {
+  console.error('Error reading user.json:', err);
 }
 
 // Setup Handlebars
@@ -33,23 +45,12 @@ app.set('views', path.join(__dirname, '..', 'views'));
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(
-  sesh({
-    secret: 'someSecretKey',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 3 * 60 * 1000, secure: false },
-  })
-);
-
-// Load users
-const userFile = path.join(__dirname, '..', 'user.json');
-let users = {};
-try {
-  users = JSON.parse(fs.readFileSync(userFile, 'utf8'));
-} catch (err) {
-  console.error('Error reading user.json:', err);
-}
+app.use(session({
+  secret: 'someSecretKey',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 3 * 60 * 1000, secure: false },
+}));
 
 // Auth middleware
 function Login(req, res, next) {
@@ -57,15 +58,26 @@ function Login(req, res, next) {
   next();
 }
 
-// Setup borrow/return router after DB connected
-connectToDatabase().then((db) => {
-  borrowReturnSetup({ database: db, loginMiddleware: Login });
-  app.use('/', borrowReturnRouter);
+// Basic ping test route
+app.get('/ping', (req, res) => res.send('pong'));
+
+// Borrow/Return router setup (lazy loaded)
+const { router: borrowReturnRouter, setup: borrowReturnSetup } = require('../routes/borrowReturnRouter');
+app.use(async (req, res, next) => {
+  if (!app.locals.borrowRouterInitialized) {
+    try {
+      const db = await connectToDatabase();
+      borrowReturnSetup({ database: db, loginMiddleware: Login });
+      app.use('/', borrowReturnRouter);
+      app.locals.borrowRouterInitialized = true;
+    } catch (e) {
+      console.error('Router setup failed:', e);
+    }
+  }
+  next();
 });
 
 // Routes
-app.get('/ping', (req, res) => res.send('pong'));
-
 app.get('/', (req, res) => res.render('landing'));
 
 app.get('/signin', (req, res) => res.render('signin', { error: null }));
@@ -81,37 +93,33 @@ app.post('/signin', (req, res) => {
 });
 
 app.get('/home', Login, async (req, res) => {
+  const db = await connectToDatabase();
   const username = req.session.user;
-  try {
-    const db = await connectToDatabase();
-    const books = await db.collection('library').find({}).toArray();
-    const clientDoc = await db.collection('clients').findOne({ username });
-    const borrowedIDs = clientDoc?.IDBooksBorrowed || [];
 
-    const borrowedBooks = books.filter(book =>
-      borrowedIDs.some(id => id.toString() === book._id.toString())
-    );
+  const books = await db.collection('library').find({}).toArray();
+  const clientDoc = await db.collection('clients').findOne({ username });
+  const borrowedIDs = clientDoc?.IDBooksBorrowed || [];
 
-    const availableBooks = books.filter(book =>
-      book.available && !borrowedIDs.some(id => id.toString() === book._id.toString())
-    );
+  const borrowedBooks = books.filter(book =>
+    borrowedIDs.some(id => id.toString() === book._id.toString())
+  );
 
-    res.render('home', {
-      username,
-      borrowedBooks: borrowedBooks.map(b => ({ ...b, _id: b._id.toString() })),
-      availableBooks: availableBooks.map(b => ({ ...b, _id: b._id.toString() })),
-    });
-  } catch (err) {
-    console.error('Error fetching books or client info:', err);
-    res.status(500).send('Internal Server Error');
-  }
+  const availableBooks = books.filter(book =>
+    book.available && !borrowedIDs.some(id => id.toString() === book._id.toString())
+  );
+
+  res.render('home', {
+    username,
+    borrowedBooks: borrowedBooks.map(b => ({ ...b, _id: b._id.toString() })),
+    availableBooks: availableBooks.map(b => ({ ...b, _id: b._id.toString() })),
+  });
 });
 
 app.get('/signout', (req, res) => {
   req.session.destroy(() => res.redirect('/'));
 });
 
-// Local only
+// Local server only
 if (!isVercel) {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
